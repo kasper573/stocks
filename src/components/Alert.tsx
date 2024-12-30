745cbf3fedabce0bd07af32d1d1a368390f7895c
Kasper <805006+kasper573@users.noreply.github.com>
2024-12-30 06:20:04 +0100

    react howler

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,12 +1,11 @@
+import Audio from "react-howler";
 import alertUrl from "../assets/alert.mp3";
 
 export function Alert({ onDismiss }: { onDismiss?: () => void }) {
   return (
     <dialog open>
       <h1>Alert!</h1>
-      <audio autoPlay loop>
-        <source src={alertUrl} type="audio/mp3" />
-      </audio>
+      <Audio src={alertUrl} playing loop volume={0.5} />
       <button onClick={onDismiss}>Dismiss</button>
     </dialog>
   );
