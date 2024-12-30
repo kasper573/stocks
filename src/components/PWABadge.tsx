51621a1f8459e1b6ebce7a8ea52ba16ef3dee6ef
Kasper <805006+kasper573@users.noreply.github.com>
2024-12-30 06:50:47 +0100

    remove periodic sync code

--- src/components/PWABadge.tsx
+++ src/components/PWABadge.tsx
@@ -3,26 +3,10 @@ import "./PWABadge.css";
 import { useRegisterSW } from "virtual:pwa-register/react";
 
 export function PWABadge() {
-  // periodic sync is disabled, change the value to enable it, the period is in milliseconds
-  // You can remove onRegisteredSW callback and registerPeriodicSync function
-  const period = 0;
-
   const {
     needRefresh: [needRefresh, setNeedRefresh],
     updateServiceWorker,
-  } = useRegisterSW({
-    onRegisteredSW(swUrl, r) {
-      if (period <= 0) return;
-      if (r?.active?.state === "activated") {
-        registerPeriodicSync(period, swUrl, r);
-      } else if (r?.installing) {
-        r.installing.addEventListener("statechange", (e) => {
-          const sw = e.target as ServiceWorker;
-          if (sw.state === "activated") registerPeriodicSync(period, swUrl, r);
-        });
-      }
-    },
-  });
+  } = useRegisterSW();
 
   function close() {
     setNeedRefresh(false);
@@ -52,29 +36,4 @@ export function PWABadge() {
       )}
     </div>
   );
-}
-
-/**
- * This function will register a periodic sync check every hour, you can modify the interval as needed.
- */
-function registerPeriodicSync(
-  period: number,
-  swUrl: string,
-  r: ServiceWorkerRegistration,
-) {
-  if (period <= 0) return;
-
-  setInterval(async () => {
-    if ("onLine" in navigator && !navigator.onLine) return;
-
-    const resp = await fetch(swUrl, {
-      cache: "no-store",
-      headers: {
-        cache: "no-store",
-        "cache-control": "no-cache",
-      },
-    });
-
-    if (resp?.status === 200) await r.update();
-  }, period);
 }
