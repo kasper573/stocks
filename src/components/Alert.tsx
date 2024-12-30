6fdb35c7ae8d4d903ff9ad6bdd254f4dea8b9e72
Kasper <805006+kasper573@users.noreply.github.com>
2024-12-30 05:27:40 +0100

    fix: make alert more mobile friendly

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -3,7 +3,7 @@ import alertUrl from "../assets/alert.mp3";
 export function Alert({ onDismiss }: { onDismiss?: () => void }) {
   return (
     <dialog open>
-      <h1>target percentage observed!</h1>
+      <h1>Alert!</h1>
       <audio autoPlay loop>
         <source src={alertUrl} type="audio/mp3" />
       </audio>
